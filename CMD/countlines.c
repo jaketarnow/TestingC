43b7a97b5670552d41d056c16f73931ab20bb867
jaketarnow <jtarnow@dons.usfca.edu>
2016-01-22 17:36:06 -0800

    edits to countlines program

--- CMD/countlines.c
+++ CMD/countlines.c
@@ -3,27 +3,31 @@
 #include <string.h>
 
 int main() {
-	FILE *fp;
-	int numLines = 0;
-	//filename of 40 character string is max?
-	char filename[40], sampleChar;
-	//prompt use for file name
-	printf("Enter file name: ");
-	scanf("%s", filename);
-	//open file in read mode
-	fp = fopen(filename, "r");
-	//get character from file and store in var sampleChar
-	sampleChar = getc(fp);
+    FILE *fp;
+    int count = 0;
+    char filename[40];
+    char sampleChar;
+ 
+    printf("Enter file name: ");
+    scanf("%s", filename);
+ 
+    // Open the file
+    fp = fopen(filename, "r");
+ 
+    // Check if file exists
+    if (fp == NULL) {
+        printf("Could not open file %s", filename);
+        return 0;
+    }
 
-	while (sampleChar != EOF) {
-		int comp = strncmp(sampleChar, "\n", "");
-		if (comp == 0) {
-			numLines = numLines + 1;
-		}
-		sampleChar = getc(fp);
-	}
-	//close file
-	fclose(fp);
-	printf("There are %d lines in %s", numLines, filename);
-	return 0;
+    for (sampleChar = getc(fp); sampleChar != EOF; sampleChar = getc(fp)) {
+    	if (sampleChar == '\n') {
+    		count = count + 1;
+    	}
+    }
+    // Close the file
+    fclose(fp);
+    printf("The file %s has %d lines\n", filename, count);
+ 
+    return 0;
 }
